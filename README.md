21652b1d4fc2abb30e33db872da2b28edee728dd
ETBlue <etblue.tw@gmail.com>
2014-12-27 19:37:46 +0800

    setup and create mockup

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ COSCUPer
 ========
 
 COSCUP Personal Data Center
+
+hackopad: https://g0v.hackpad.com/COSCUPer-COSCUP-Personal-Data-Center-apyH3KIENaa
+
---
+++ public/js/speaker-list.js
@@ -0,0 +1,15 @@
+(function(){
+  [{
+    "id": "1",
+    "content": "總統有權無責(他可以任意作他想作的，大家不能怎麼樣)",
+    "class": ['問題'],
+    "tag": ['憲法'],
+    "rating": ['3'],
+    "agree": [],
+    "disagree": [],
+    "block": [],
+    "info": [],
+    "source": [],
+    "parent": []
+  }];
+}).call(this);
